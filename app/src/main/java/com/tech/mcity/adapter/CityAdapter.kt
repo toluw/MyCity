f97a54621acfa23b30d58eed3c67b7c8969fb8e6
toluw <oketoluwase@gmail.com>
2022-04-02 21:35:01 +0100

    Implement RecyclerView ViewHolder

---
+++ app/src/main/java/com/tech/mcity/adapter/CityAdapter.kt
@@ -0,0 +1,55 @@
+package com.tech.mcity.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.tech.mcity.R
+import com.tech.mcity.data.model.City
+
+class CityAdapter():
+    PagingDataAdapter<City, CityAdapter.CityViewHolder>(CityComparator){
+
+        class CityViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+            val cityTextView: TextView = itemView.findViewById(R.id.cityTextView)
+            val countryTextView: TextView = itemView.findViewById(R.id.countryTextView)
+
+        }
+
+
+       object CityComparator : DiffUtil.ItemCallback<City>() {
+        override fun areItemsTheSame(oldItem: City, newItem: City): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: City, newItem: City): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+
+
+    override fun onBindViewHolder(holder: CityViewHolder, position: Int) {
+
+        holder.cityTextView.text = getItem(position)!!.name
+        holder.countryTextView.text = getItem(position)!!.country
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
+        return CityViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.city_layout,
+                parent,
+                false
+            )
+        )
+    }
+
+
+}
